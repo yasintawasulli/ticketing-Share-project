b89444c7d318d97814c41a7502b1c5b795b6fd25
yasinTawasulli <royesh14@gmail.com>
2022-12-14 13:13:19 -0800

    Initial commit

---
+++ src/main/java/com/cydeo/TicketingShareProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingShareProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingShareProjectApplication.class, args);
+    }
+
+}
