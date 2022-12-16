3b42712722b2612d7b220ca7604bc938a7e064bb
jawidkarimi <104651569+jawidkarimi@users.noreply.github.com>
2022-12-15 22:05:03 -0800

    Mapper commented

---
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.cydeo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/api/v1/user")
+public class UserController {
+
+ public ResponseEntity<ResponsW>
+
+}
